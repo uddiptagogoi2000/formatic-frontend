f36f0879d23bcf5265ebd6dc5cf58492b3abc728
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2024-06-27 23:29:48 +0530

    feat: Refactor withConfig hoc to use elementDetails instead of elementProperties
    Add Config Buttons and Styles for the same. Also use useFormContext hook to get the state and onSelectElement from the context.
    
    Implement HandleSelect Element which selects an element and give borders to the element.

--- src/components/hoc/withConfig.tsx
+++ src/components/hoc/withConfig.tsx
@@ -1,38 +1,70 @@
-import { ElementPropertiesByCategory } from "../../contexts/formContext";
+import { useEffect } from "react";
+import { Element as ElementDetails } from "../../contexts/apptypes";
+import { useFormContext } from "../../contexts/formContext";
+import "./styles.css";
 
-type ElementProps = {
-  elementProperties: ElementPropertiesByCategory;
+export type ElementProps = {
+  elementDetails: ElementDetails; // temporarily optional
+  // removeElement: () => void;
+  // showProperties: () => void;
+  // dragElement: () => void;
+  // blurElement: () => void;
+  // updateElement: () => void;
 };
 
-function configMachine(state: any, event: any) {
-  switch (state) {
-    case "focus": {
-      if (event.type === "SHOW_PROPERTIES") {
-      }
+// function configMachine(state: any, event: any) {
+//   switch (state) {
+//     case "focus": {
+//       if (event.type === "SHOW_PROPERTIES") {
+//       }
 
-      if (event.type === "REMOVE") {
-      }
+//       if (event.type === "REMOVE") {
+//       }
 
-      if (event.type === "DRAG") {
-      }
+//       if (event.type === "DRAG") {
+//       }
 
-      if (event.type === "BLUR") {
-      }
-    }
-    case "blur": {
-    }
-  }
-}
+//       if (event.type === "BLUR") {
+//       }
+//     }
+//     case "blur": {
+//     }
+//   }
+// }
 
 const withConfig = <P extends object>(
   Component: React.FC<P & ElementProps>
 ) => {
-  const WrappedComponent = (props: P & ElementProps) => {
+  const WrappedComponent = (props: ElementProps) => {
+    // has access to elementProperties
+    // so we can pass it down to the component
+    // also we can properties to components that are in upper hierarchy using callbacks
+
+    const { state, onSelectElement } = useFormContext();
+
+    const isSelected = state.selectedElementId === props.elementDetails?.id;
+
+    function handleSelect(elementId: number) {
+      onSelectElement(elementId);
+    }
+
+    function handleOpenSettingsPanel() {}
+
     return (
-      <div>
+      <div
+        className={`element-wrapper ${
+          isSelected ? "element-wrapper--selected" : ""
+        }`}
+        onClick={() => handleSelect(props.elementDetails.id)}
+      >
+        <div className='element-wrapper__config-button-group'>
+          <button onClick={handleOpenSettingsPanel}>Settings</button>
+          <button>Remove</button>
+        </div>
         <Component
           {...(props as P)}
-          elementProperties={props.elementProperties}
+          elementDetails={props.elementDetails}
+          isSelected={isSelected}
         />
       </div>
     );
