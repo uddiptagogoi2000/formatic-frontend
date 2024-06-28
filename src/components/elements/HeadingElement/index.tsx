ffc58165c7f4636f02bc86de48b74d6391d09a72
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2024-06-28 23:56:25 +0530

    feat: Refactor HeadingElement Component logic.
    1. It now uses a Html Input Elements instead of contenteditable.: It seems like there is no use of contenteditable element for now and it was adding extra overhead logic to manage states.
    
    2. Wrote handleClickOutside function: for HeadingElement and also the clean up function to update the previously selected element.
    
    3. Used extra refs:  to have the fresh states inside the clean up function. It was having old state state from the time when loaded first that's why used extra refs for the states.
    
    4. Had to write separate effects for isSelected and [headingTextState, subHeadingTextState]  because it was overlapping and due to this updateElement was being called infinitely when typing.
    
    5. todo: Refactor

--- src/components/elements/HeadingElement/index.tsx
+++ src/components/elements/HeadingElement/index.tsx
@@ -1,7 +1,12 @@
-import { useEffect, useReducer, useRef } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import withConfig from "../../hoc/withConfig";
 import "./styles.css";
-import { Element as ElementDetails } from "../../../contexts/apptypes";
+import {
+  Element as ElementDetails,
+  Property,
+  TextProperty,
+} from "../../../contexts/apptypes";
+import { useFormContext } from "../../../contexts/formContext";
 
 function len(text: string) {
   return text.length;
@@ -66,23 +71,47 @@ type HeadingElementProps = {
   isSelected: boolean;
 };
 
-const useHeadingElementValues = (details: ElementDetails) => {
-  let headingText = "Heading";
-  let subHeadingText = "";
+const useHeadingElementValues = (
+  details: ElementDetails
+): {
+  headingTextProperty: TextProperty;
+  subHeadingTextProperty: TextProperty;
+} => {
+  let headingTextProperty: TextProperty | undefined = undefined;
+  let subHeadingTextProperty: TextProperty | undefined = undefined;
 
   details.properties.forEach((property) => {
     if (property.name === "Heading Text" && property.type === "text") {
       // todo: make the strings Variables or Enums
-      headingText = property.value;
+      headingTextProperty = property;
     }
 
     if (property.name === "Subheading Text" && property.type === "text") {
-      subHeadingText = property.value;
+      subHeadingTextProperty = property;
     }
   });
 
-  return { headingText, subHeadingText };
+  if (!headingTextProperty) {
+    throw new Error("Heading Text property not found");
+  }
+
+  if (!subHeadingTextProperty) {
+    throw new Error("Subheading Text property not found");
+  }
+
+  return { headingTextProperty, subHeadingTextProperty };
 };
+
+// function setCaretPosition(element, position) {
+//   const range = document.createRange();
+//   const selection = window.getSelection();
+//   range.setStart(element.childNodes[0], position);
+//   range.collapse(true);
+//   selection.removeAllRanges();
+//   selection.addRange(range);
+// }
+
+// let's write it in typescript
 
 const HeadingElement = ({
   elementDetails,
@@ -93,17 +122,37 @@ const HeadingElement = ({
   const contentEditableDivRef = useRef(null);
   const headingTextRef = useRef(null);
   const subHeadingTextRef = useRef(null);
+  const { onUpdateElement, state } = useFormContext();
   // dummy: const updateElement = useContext(formContext)
-
-  const initialState: State = {
-    [IS_HEADING_EMPTY]: false,
-    [IS_SUBHEADING_EMPTY]: true,
-  };
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { isHeadingEmpty, isSubHeadingEmpty } = state;
-  const { headingText, subHeadingText } =
+  const { headingTextProperty, subHeadingTextProperty } =
     useHeadingElementValues(elementDetails);
+
+  // console.log("headingText", headingText);
+  // console.log("subHeadingText", subHeadingText);
+
+  const [headingTextState, setHeadingTextState] = useState(
+    headingTextProperty.value
+  );
+  const [subHeadingTextState, setSubHeadingTextState] = useState(
+    subHeadingTextProperty.value
+  );
+
+  const headingTextValueRef = useRef("");
+  const subHeadingTextValueRef = useRef("");
+
+  // console.log("headingTextState", headingTextState);
+  // console.log("subHeadingTextState", subHeadingTextState);
+
+  // const isHeadingEmpty = len(headingTextState) === 0;
+  // const isSubHeadingEmpty = len(subHeadingTextState) === 0;
+
+  // const initialState: State = {
+  //   [IS_HEADING_EMPTY]: headingText.length === 0,
+  //   [IS_SUBHEADING_EMPTY]: subHeadingText.length === 0,
+  // };
+
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  // const { isHeadingEmpty, isSubHeadingEmpty } = state;
 
   // elementProperties[GENERAL].states[HEADING_TEXT]
   // elementProperties[GENERAL].states[SUBHEADING_TEXT]
@@ -124,70 +173,195 @@ const HeadingElement = ({
   //   }
   // });
 
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (headingTextRef.current !== event.target) {
-        console.log("You clicked outside");
-      }
-    }
+  // console.log("iscurrentlySelected", elementDetails.id, isSelected);
+  // console.log("isPreviouslySelected", elementDetails.id, isPreviouslySelected);
 
+  function handleClickOutside(event: MouseEvent) {
+    // if (headingTextRef.current !== event.target) {
+    //   console.log("You clicked outside");
+    //   onUpdateElement(elementDetails.pageId, elementDetails.id, {
+    //     ...headingTextProperty,
+    //     value: headingTextState,
+    //   });
+    // }
+    if (headingTextRef.current !== event.target && isSelected) {
+      console.log(
+        "You clicked outside, here we are",
+        "heading of element",
+        elementDetails.id
+      );
+      onUpdateElement(elementDetails.pageId, elementDetails.id, {
+        ...headingTextProperty,
+        value: headingTextState,
+      });
+    }
+    if (subHeadingTextRef.current !== event.target && isSelected) {
+      console.log(
+        "You clicked outside, here we are",
+        "subheading of element",
+        elementDetails.id
+      );
+      onUpdateElement(elementDetails.pageId, elementDetails.id, {
+        ...subHeadingTextProperty,
+        value: subHeadingTextState,
+      });
+    }
+  }
+
+  useEffect(() => {
+    headingTextValueRef.current = headingTextState;
+  }, [headingTextState]);
+
+  useEffect(() => {
+    subHeadingTextValueRef.current = subHeadingTextState;
+  }, [subHeadingTextState]);
+
+  useEffect(() => {
     if (isSelected) {
+      console.log("adding event listener of ", elementDetails.id);
       document.addEventListener("click", handleClickOutside);
     }
 
-    () => {
+    return () => {
+      console.log("removing event listener of ", elementDetails.id);
       document.removeEventListener("click", handleClickOutside);
 
-      // Update the element in the form context
-      // send innertext of heading and subheading to the form context by calling updateElement
+      const elementId = elementDetails.id;
+      const pageId = elementDetails.pageId;
+
+      if (isSelected) {
+        onUpdateElement(pageId, elementId, {
+          ...headingTextProperty,
+          value: headingTextValueRef.current,
+        });
+        onUpdateElement(pageId, elementId, {
+          ...subHeadingTextProperty,
+          value: subHeadingTextValueRef.current,
+        });
+      }
     };
   }, [isSelected]);
+
+  useEffect(() => {
+    if (isSelected) {
+      console.log("adding event listener for when typing", elementDetails.id);
+      document.addEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      console.log("removing event listener for when typing", elementDetails.id);
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isSelected, headingTextState, subHeadingTextState]);
+
+  useEffect(() => {
+    setHeadingTextState(headingTextProperty.value);
+    setSubHeadingTextState(subHeadingTextProperty.value);
+  }, [subHeadingTextProperty.value, headingTextProperty.value]);
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: "input_changed",
+  //     payload: {
+  //       innerText: headingText,
+  //       prop: IS_HEADING_EMPTY,
+  //     },
+  //   });
+  // }, [headingText]);
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: "input_changed",
+  //     payload: {
+  //       innerText: subHeadingText,
+  //       prop: IS_SUBHEADING_EMPTY,
+  //     },
+  //   });
+  // }, [subHeadingText]);
+
+  // useEffect(() => {
+  //   if (subHeadingTextRef.current !== null) {
+  //     const subHeadingTextElement = subHeadingTextRef.current as HTMLElement;
+
+  //     subHeadingTextElement.innerText = subHeadingText;
+  //   }
+  // }, [subHeadingText]);
+
+  // console.log("rendering", elementDetails.properties);
 
   return (
     <div ref={contentEditableDivRef} className='heading-element element'>
       <h2
         spellCheck='false'
-        ref={headingTextRef}
-        contentEditable='plaintext-only'
+        // ref={headingTextRef}
+        // contentEditable='plaintext-only'
         suppressContentEditableWarning
-        className={`heading-element__heading editable ${
-          isHeadingEmpty ? "inlineEditEmpty" : ""
-        }`}
+        className={`heading-element__heading editable`}
         data-placeholder='Type a header'
-        onInput={(e) => {
-          const target = e.target as HTMLElement;
-          dispatch({
-            type: "input_changed",
-            payload: {
-              innerText: target.innerText,
-              prop: IS_HEADING_EMPTY,
-            },
-          });
-        }}
+        // onInput={(e) => {
+        //   const target = e.target as HTMLElement;
+        //   dispatch({
+        //     type: "input_changed",
+        //     payload: {
+        //       innerText: target.innerText,
+        //       prop: IS_HEADING_EMPTY,
+        //     },
+        //   });
+        // }}
+
+        // onInput={(e) => {
+        //   setHeadingTextState(e.currentTarget.innerText);
+        //   setCaretPosition(e.currentTarget, len(e.currentTarget.innerText));
+        // }}
       >
-        {headingText}
+        {/* {headingTextState} */}
+        <input
+          type='text'
+          value={headingTextState}
+          ref={headingTextRef}
+          placeholder='Type a header'
+          onChange={(e) => {
+            setHeadingTextState(e.target.value);
+          }}
+        />
       </h2>
       <div
+        // style={{
+        //   border: "1px solid black",
+        // }}
         spellCheck='false'
-        ref={subHeadingTextRef}
-        contentEditable='plaintext-only'
+        // ref={subHeadingTextRef}
+        // contentEditable='plaintext-only'
         suppressContentEditableWarning
-        className={`heading-element__subheading editable ${
-          isSubHeadingEmpty ? "inlineEditEmpty" : ""
-        }`}
+        className={`heading-element__subheading editable`}
         data-placeholder='Type a subheader'
-        onInput={(e) => {
-          const target = e.target as HTMLElement;
-          dispatch({
-            type: "input_changed",
-            payload: {
-              innerText: target.innerText,
-              prop: IS_SUBHEADING_EMPTY,
-            },
-          });
-        }}
+        // data-value={subHeadingText}
+        // onInput={(e) => {
+        //   const target = e.target as HTMLElement;
+        //   dispatch({
+        //     type: "input_changed",
+        //     payload: {
+        //       innerText: target.innerText,
+        //       prop: IS_SUBHEADING_EMPTY,
+        //     },
+        //   });
+        // }}
+
+        // onInput={(e) => {
+        //   setSubHeadingTextState(e.currentTarget.innerText);
+        //   setCaretPosition(e.currentTarget, len(e.currentTarget.innerText));
+        // }}
       >
-        {subHeadingText}
+        {/* {subHeadingTextState} */}
+        <input
+          type='text'
+          value={subHeadingTextState}
+          ref={subHeadingTextRef}
+          placeholder='Type a subheader'
+          onChange={(e) => {
+            setSubHeadingTextState(e.target.value);
+          }}
+        />
       </div>
     </div>
   );
